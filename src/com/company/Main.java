fa47ac201fd5132c9195aa6f77d2c37f0684dfc4
JR <jrdotat@gmail.com>
2019-12-07 00:11:03 -0800

    Added basic structure and visual; No functionality

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,7 +2,6 @@ package com.company;
 
 import javax.swing.*;
 
-
 public class Main {
 
     public static void main(String[] args) {
@@ -14,13 +13,12 @@ public class Main {
         linkInput.setBounds(50, 90, 250, 35);
         JButton getPhoto = new JButton("Get photo");
         getPhoto.setBounds(125,125, 100, 35);
-
+        
         f.add(getPhoto);
         f.add(linkInput);
         f.setSize(350, 300);
         f.setLayout(null);
         f.setVisible(true);
-
 
     }
 
