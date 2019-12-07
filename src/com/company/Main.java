10d6407aedea35acef16125eb644e8e02ed7b847
JR <jrdotat@gmail.com>
2019-12-07 00:07:51 -0800

    Added basic structure and visual; No functionality

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,27 @@
 package com.company;
 
+import javax.swing.*;
+
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        SpringLayout sl = new SpringLayout();
+
+        JFrame f = new JFrame();
+        JTextField linkInput = new JTextField();
+        linkInput.setBounds(50, 90, 250, 35);
+        JButton getPhoto = new JButton("Get photo");
+        getPhoto.setBounds(125,125, 100, 35);
+
+        f.add(getPhoto);
+        f.add(linkInput);
+        f.setSize(350, 300);
+        f.setLayout(null);
+        f.setVisible(true);
+
+
     }
+
 }
